c941cfd975d43e4499262cf09c0040cdbedcffb3
Lorenzo Zana <lorenzozana@gmail.com>
2022-04-28 10:52:45 -0400

    Adding file for test

--- writeToRoot.C
+++ writeToRoot.C
@@ -1,5 +1,5 @@
 {
-  TFile *MyFile = new TFile("hallB_geant4_simmetric.root","NEW");
+  TFile *MyFile = new TFile("hallB_geant4_dump.root","NEW");
   gSystem->Load("libGeom");
   TGeoManager *geo = new TGeoManager("World", "HallB Geant4");
   geo->Import("output.gdml");
